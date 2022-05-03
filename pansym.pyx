22c64f44848456723a0beb87d991989aab812e56
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2022-05-03 16:35:20 +0200

    todo

--- pansym.pyx
+++ pansym.pyx
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-# in python right now, convert to cython later
+# python right now, convert to cython later
+
+"""
+This file will find pansyntenic regions, either to use in multiple structural variant calling or for separate analysis.
+"""
 
 def find_pansyn(coords):
-
+    #TODO actually find pansynteny:
+    #   - use pairwise to ref or multiple alignments?
+    #       => start with pairwise to ref
+    #   - identify syntenic regions to ref, possibly reusing code from syri(synsearch.pyx, ll. 500-550)
+    #   - output in file/plot in some clever way
+    #       - maybe as chromosome "bands"?
+    #       - maybe as % syntenic to each query? => clustering/rooted tree
