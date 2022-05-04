f4a54277670591707a88d22e4e01214aa4ae78c9
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2022-05-04 16:52:36 +0200

    intersection implemented naively

--- pansym.pyx
+++ pansym.pyx
@@ -4,33 +4,47 @@
 """
 This file will find pansyntenic regions, either to use in multiple structural variant calling or for separate analysis.
 """
-
-# coords is a list of coordinate pandas frames as output by the readCoords function.
-# A is the reference and B the query genome
-#TODO allow loading multiple bam files in main.py
-
-def find_pansyn(coords):
-    #TODO actually find pansynteny:
-    #   - use pairwise to ref AND SyRI output (TODO loading)
-    #   - identify syntenic regions to ref, possibly reusing code from syri(synsearch.pyx, ll. 500-550)
-    #       - find regions sharing similar locations on the A genome, matching on aStart?
-    #       - sorted join type algorithm?
-    #       - lifting pansyntenic regions? if lifting, what topology?
-    #   - FIRST see if simple intersectionworks, using np.intersect1d
-    #   - output in file/plot in some clever way
-    #       - maybe as chromosome "bands"?
-    #       - maybe as % syntenic to each query? => clustering/rooted tree
-    pass
-
-
-
-
-def extract_syn_regions(df):
-    return df.loc[df['type']=='SYN']
-
-
+import functools
+import pandas as pd
 import sys
 import ingest
 
-test, _ = ingest.readsyriout(sys.argv[1])
-print(extract_syn_regions(test))
+
+#TODO actually find pansynteny:
+#   - use pairwise to ref AND SyRI output (TODO loading)
+#   - identify syntenic regions to ref, possibly reusing code from syri(synsearch.pyx, ll. 500-550)
+#       - find regions sharing similar locations on the A genome, matching on aStart?
+#       - sorted join type algorithm?
+#       - lifting pansyntenic regions? if lifting, what topology?
+#   - FIRST see if simple intersectionworks, using np.intersect1d
+#   - output in file/plot in some clever way
+#       - maybe as chromosome "bands"?
+#       - maybe as % syntenic to each query? => clustering/rooted tree
+
+#TODO allow loading multiple bam files in main.py
+
+def find_pansyn(fins, ref="a", qry="b"):
+    """
+    TODO docs
+    :param:
+    :return:
+    """
+    # columns to look for as start/end positions
+    refstart = ref + "start"
+    refend = ref + "end"
+    qrystart = qry + "start"
+    qryend = qry + "end"
+
+    def extract_syn_regions(df):
+        return df.loc[df['type']=='SYN'][["achr", "astart", "aend"]]
+
+    syns = [extract_syn_regions(ingest.readsyriout(fin)[0]) for fin in fins]
+    
+    pansyns = functools.reduce(pd.merge, syns)
+
+    return len(pansyns)
+
+
+# helper function to extract only large, syntenic regions suitable for analysis from a DF
+
+print(find_pansyn(sys.argv[1:]))
