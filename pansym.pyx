92d9f8e4ed972c2c0810997bedd44ee74c85c5b2
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2022-05-05 11:16:15 +0200

    preliminary implementation of subregion, tolerant matching

--- pansym.pyx
+++ pansym.pyx
@@ -6,6 +6,7 @@ This file will find pansyntenic regions, either to use in multiple structural va
 """
 import functools
 import pandas as pd
+import numpy as np
 import sys
 import ingest
 
@@ -28,26 +29,59 @@ def find_pansyn(fins, ref="a", qry="b"):
     :return:
     """
     # columns to look for as start/end positions
+    refchr = ref + "chr"
     refstart = ref + "start"
     refend = ref + "end"
-    refchr = ref + "chr"
+    qrychr = qry + "chr"
     qrystart = qry + "start"
     qryend = qry + "end"
-    qrychr = qry + "chr"
 
     # helper function to extract only large, syntenic regions suitable for analysis from a DF
     def extract_syn_regions(df):
         return df.loc[df['type']=='SYN'][[refchr, refstart, refend]]
 
     syns = [extract_syn_regions(ingest.readsyriout(fin)[0]) for fin in fins]
-    
+    # take two dataframes of syntenic regions and compute the flexible intersection
+    def intersect_syns(left, right, tolerance=10):
+        #return pd.merge(left, right) # equivalent to old strict intersection
+        
+        # they should already be sorted -- required for the rest of the algorithm
+        #left.sort_values([refchr, refstart, refend])
+        #right.sort_values([refchr, refstart, refend])
+
+        ret = pd.DataFrame(columns = left.columns) # or delete from left? might be faster
+        ret = []
+
+        #print(left)
+        #print(right)
+        riter = right.iterrows()
+        for lsyn in left.iterrows():
+            try:
+                rsyn = next(riter)
+            except StopIteration:
+                break
+            # for some reason, this iterates over tuples with the index
+            lsyn = lsyn[1]
+            rsyn = rsyn[1]
+
+            if lsyn[refchr] == rsyn[refchr]:
+                # left subregion of right
+                if lsyn[refstart] > rsyn[refstart] - tolerance and \
+                    lsyn[refend] < rsyn[refend] + tolerance:
+                        #ret = pd.concat([ret, lsyn])
+                        ret.append(lsyn)
+                # right subregion of left
+                elif rsyn[refstart] > lsyn[refstart] - tolerance and \
+                        rsyn[refend] < lsyn[refend] + tolerance:
+                        ret.append(rsyn)
+
+        del riter
+        return ret
+
+
     pansyns = functools.reduce(intersect_syns, syns)
 
     return len(pansyns)
-
-# take two dataframes of syntenic regions and compute the flexible intersection
-def intersect_syns(left, right):
-    return pd.merge(left, right) # equivalent to old strict intersection
 
 
 print(find_pansyn(sys.argv[1:]))
