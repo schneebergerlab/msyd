6d56933c26bdb6a692478be5f5fa6801be457cad
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2022-05-24 16:23:08 +0200

    script running (?)

--- hgsv_scripts/minimap_submit.sh
+++ hgsv_scripts/minimap_submit.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 ref=$1
-outpath=alns
+outpath="alns"
 
 for seq in scaffolds/*.fna.gz
 do
 	bs=$(basename $seq)
 	bsub -q multicore20 -n20 -R"span[hosts=1] rusage[mem=20000]" -M30000 \
-	-oo aln_$bs.log -eo aln_$bs.err \
-	"minimap2 -cx asm5 --eqx grch38.v14_genomic.fna.gz $seq > $outpath/$bs.paf"
+		-oo aln_$bs.log -eo aln_$bs.err \
+		"minimap2 -cx asm5 --eqx grch38.v14_genomic.fna.gz $seq > $outpath/$bs.paf"
 done
