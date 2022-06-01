c4304aba9e8dde091bed9cb2a9ea921fb64de767
Leon Rauschning <leon.rauschning@tum.de>
2022-06-01 17:58:01 +0200

    submit

--- hgsv_scripts/syri_submit.sh
+++ hgsv_scripts/syri_submit.sh
@@ -4,6 +4,6 @@ for seq in scaffolds/*.fna.gz
 do
 	bs=$(basename $seq)
 	bsub -q multicore20 -n20 -R"span[hosts=1] rusage[mem=6000]" -M10000 \
-	-oo ${tmpdir}syri_$ref$org.log -eo ${tmpdir}syri_$ref$org.err \
+	-oo job_syri_${bs}.log -eo job_syri_${bs}.err \
 	"syri --nc 20 -F P --cigar --prefix syri/$bs -c alns/$bs.paf -r grch38.v14_genomic.fna.gz -q $seq --lf $bs.syri.log"
 done
