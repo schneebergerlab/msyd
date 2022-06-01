800d6a192040c6031f582fb445ad394218157eae
Leon Rauschning <leon.rauschning@tum.de>
2022-06-01 11:57:41 +0200

    refactored out cigar, untested

--- cigar.pyx
+++ cigar.pyx
@@ -4,8 +4,6 @@
 # cython: language_level = 3
 import re
 import copy
-import multiprocessing
-import functools
 import itertools
 
 ## constants
@@ -224,26 +222,6 @@ class Cigar:
 
 
         
-
-
-# copied from https://stackoverflow.com/questions/50878960/parallelize-pythons-reduce-command
-# doesn't seem to be very fast?
-def parallel_reduce(reduceFunc, l, numCPUs):
-    if numCPUs == 1 or len(l) <= 100:
-            returnVal = functools.reduce(reduceFunc, l[1:], l[0])
-            return returnVal
-
-    parent1, child1 = multiprocessing.Pipe()
-    parent2, child2 = multiprocessing.Pipe()
-    p1 = multiprocessing.Process(target=parallel_reduce, args=(reduceFunc, l[:len(l) // 2], numCPUs // 2, child1, ) )
-    p2 = multiprocessing.Process(target=parallel_reduce, args=(reduceFunc, l[len(l) // 2:], numCPUs // 2 + numCPUs%2, child2, ) )
-    p1.start()
-    p2.start()
-    leftReturn, rightReturn = parent1.recv(), parent2.recv()
-    p1.join()
-    p2.join()
-    returnVal = reduceFunc(leftReturn, rightReturn)
-    return returnVal
 
 
 if __name__ == "__main__":
--- pansyn.pyx
+++ pansyn.pyx
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 import ingest
 import util
-from util import Cigar
+from cigar import Cigar
 
 
 
