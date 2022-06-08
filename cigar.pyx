5095b7f5689820056b8af772bf2b3602a66b40dc
Leon Rauschning <leon.rauschning@tum.de>
2022-06-08 11:42:23 +0200

    removed print

--- cigar.pyx
+++ cigar.pyx
@@ -107,7 +107,7 @@ class Cigar:
         # loop and remove regions as long as the skip is more than one region
         while ind < len(self.pairs):
             cgi = self.pairs[ind] if start else self.pairs[-ind -1]
-            print(ind, cgi, n, skip)
+            #print(ind, cgi, n, skip)
             if cgi[1] not in fwd:
                 ind += 1
                 if cgi[1] in altfwd:
