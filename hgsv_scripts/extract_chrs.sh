6e77938ea7bf70825caea9c4caecd2c92449edbe
Leon Rauschning <leon.rauschning@tum.de>
2022-06-08 17:57:10 +0200

    added bgz extension

--- hgsv_scripts/extract_chrs.sh
+++ hgsv_scripts/extract_chrs.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 tmp="/tmp/"
 out="scaffolds_filtered"
-file=$(basename $1)
+file=$(basename $1 .bgz)
 
 # determine chr list from reference
 chrs=$(zgrep -e ">NC" grch38.v14_genomic.fna.gz | sed "s/>//" | cut -f 1 -d " " | sed -z "s/\n/\ /g")
 
-zcat scaffolds/$file | sed "s/_RagTag$//" > $tmp/$file.tmp
+zcat scaffolds/$file.bgz | sed "s/_RagTag$//" > $tmp/$file.tmp
 
 hometools getchr --chrs $chrs -o scaffolds_filtered/$file $tmp/$file.tmp
 rm $tmp/$file.tmp
 bgzip --threads 4 scaffolds_filtered/$file
-
+mv scaffolds_filtered/$file.gz scaffolds_filtered/$file.bgz
