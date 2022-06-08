74042d92df624b73d88fe3ccae204c496f9b2317
Leon Rauschning <leon.rauschning@tum.de>
2022-06-08 18:05:01 +0200

    fixed typo

--- hgsv_scripts/extract_submit.sh
+++ hgsv_scripts/extract_submit.sh
@@ -5,5 +5,5 @@ do
 	bs=$(basename $seq)
 	bsub -q normal -n4 -R"span[hosts=1] rusage[mem=8000]" -M10000 \
 	-oo job_extract_${bs}.log -eo job_extract_${bs}.err \
-	"~/pansr/pansr/hgsv_scripts/extract_chr.sh $seq"
+	"~/pansr/pansr/hgsv_scripts/extract_chrs.sh $seq"
 done
