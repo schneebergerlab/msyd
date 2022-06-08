7ac4ec51b87179303f19300cb682f9b0d9e6e29e
Leon Rauschning <leon.rauschning@tum.de>
2022-06-08 18:00:33 +0200

    extract_submit.sh

---
+++ hgsv_scripts/extract_submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for seq in scaffolds/*.fna.bgz
+do
+	bs=$(basename $seq)
+	bsub -q normal -n4 -R"span[hosts=1] rusage[mem=8000]" -M10000 \
+	-oo job_extract_${bs}.log -eo job_extract_${bs}.err \
+	"~/pansr/pansr/hgsv_scripts/extract_chr.sh $seq"
+done
