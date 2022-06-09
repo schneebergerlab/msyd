63c610428209b40353315d738d9e9762dba21a06
Leon Rauschning <leon.rauschning@tum.de>
2022-06-09 11:52:54 +0200

    to no-sex reference

--- hgsv_scripts/minimap_submit.sh
+++ hgsv_scripts/minimap_submit.sh
@@ -8,5 +8,5 @@ do
 	bs=$(basename $seq)
 	bsub -q multicore20 -n20 -R"span[hosts=1] rusage[mem=20000]" -M30000 \
 		-oo aln_$bs.log -eo aln_$bs.err \
-		"minimap2 -cx asm5 --eqx ref-nomt.fna.bgz $seq > $outpath/$bs.paf"
+		"minimap2 -cx asm5 --eqx ref-nomtsex.fna.bgz $seq > $outpath/$bs.paf"
 done
