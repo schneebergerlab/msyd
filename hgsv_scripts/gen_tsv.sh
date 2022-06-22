c18591769d8f435703424a90182df82e50c389fb
Leon Rauschning <leon.rauschning@tum.de>
2022-06-22 15:11:06 +0200

    file extension added to gen_tsv.sh

--- hgsv_scripts/gen_tsv.sh
+++ hgsv_scripts/gen_tsv.sh
@@ -4,5 +4,5 @@ echo "# aln 	syri" > all.tsv
 for f in syri/*syri.out
 do
 	bs=$(basename $f syri.out)
-	echo "alns_filtered/$bs	syri/${bs}syri.out" >> all.tsv
+	echo "alns_filtered/$bs.paf	syri/${bs}syri.out" >> all.tsv
 done
