019a2bb06c68ba1ca98ddb4487ec5db0de46f8c8
Leon Rauschning <leon.rauschning@tum.de>
2022-06-24 11:08:13 +0200

    crossyn.pyx added

---
+++ crosssyn.pyx
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# distutils: language = c++
+# cython: language_level = 3
+
+import functools
+import pandas as pd
+import numpy as np
+import ingest
+import util
+from util import Range
+from cigar import Cigar
+
