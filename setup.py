f29a9c70af19edb1afd2dbae2eef9bf45e9bc5f5
Leon Rauschning <leon.rauschning@tum.de>
2022-06-24 11:06:44 +0200

    fixed bug

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import numpy
 setup(ext_modules = cythonize([
             Extension('ingest', ['ingest.pyx'], include_dirs=[numpy.get_include()]),
             Extension('coresyn', ['coresyn.pyx']),
+            Extension('crosssyn', ['crosssyn.pyx']),
             Extension('util', ['util.pyx']),
             Extension('cigar', ['cigar.pyx'])
             ]),
