57e742056eab2f66b2e54482903c9848c299c9db
Leon Rauschning <leon.rauschning@tum.de>
2022-06-27 11:45:51 +0200

    removed remcigar

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ Experimental and WIP.
 
 
 
-remcigar = lambda x: x# x[0] if type(x)==list or type(x)==tuple else x
-
-df1 = coresyn.coresyn_from_tsv(sys.argv[1], cores=int(sys.argv[2]) if len(sys.argv) >= 3 else 1).apply(lambda x: x.apply(remcigar))
+df1 = coresyn.coresyn_from_tsv(sys.argv[1], cores=int(sys.argv[2]) if len(sys.argv) >= 3 else 1)
 #print(df1.to_string())
 print("regions:", len(df1))
 print("total lengths:", sum(map(lambda x: x[1][0].end-x[1][0].start, df1.iterrows())))
