4c7c0a764f71c81d70838de23c54f2f58e35c166
Leon Rauschning <leon.rauschning@tum.de>
2022-06-27 15:45:37 +0200

    added dtypes, as automatic determination not working for dequeues?

--- syntools.pyx
+++ syntools.pyx
@@ -112,7 +112,7 @@ def match_synal(syn, aln, cons=None, ref='a'):
         except StopIteration:
             break
 
-    return pd.DataFrame(ret)
+    return pd.DataFrame(ret, dtype=cons)
 
 def extract_regions(fin, ref='a', ann='SYN', reforg='ref', qryorg='qry'):
     """
@@ -142,7 +142,7 @@ def extract_regions(fin, ref='a', ann='SYN', reforg='ref', qryorg='qry'):
             Range(qryorg, row[qrychr], qryhaplo, row[qrystart], row[qryend])
             ])
 
-    return pd.DataFrame(data=buf, columns=[reforg, qryorg])
+    return pd.DataFrame(data=buf, columns=[reforg, qryorg], dtype=Range)
 
 def extract_regions_to_list(fins, **kwargs):
     """
