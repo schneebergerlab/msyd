1aa338d9004fa36f1e703726744a332e2d6ffba8
Leon Rauschning <leon.rauschning@tum.de>
2022-06-27 18:00:18 +0200

    initial crosssyn implementation

--- crosssyn.pyx
+++ crosssyn.pyx
@@ -26,3 +26,33 @@ Do intersect-add and drop in one step, faster and less messy
 => Benchmark, may be very slow
 """
 
+class Crosssyn:
+    """
+    TODO docstring
+    """
+    # ranges, cigars have type List[Range]/List[Cigar], respectively, but cython cannot deal with generic type hints
+    def __init__(self, ref:Range, ranges, cigars):
+        self.ref = ref # optional if using a reference-free algorithm. NONE CURRENTLY IMPLEMENTED!
+        self.ranges = ranges
+        self.cigars = cigars # length equal to ranges; optional if using approximate matching
+        self.degree = len(self.ranges)
+
+    def __repr__(self):
+        return f"Crosssyn({self.ref}, {self.ranges})"
+
+    def __eq__(l, r):
+        return l.ref == r.ref and l.ranges == r.ranges and l.cigars == r.cigars
+        
+    # for now, only sorts on the reference
+    def __lt__(l, r):
+        if not l.ref or not r.ref:
+            raise ValueError(f"ERROR comparing {l} with {r}: both need to have a reference!")
+        return l.ref < r.ref
+
+    def combine(l, r):
+        #TODO implement
+        # return list or iterator instead of new object
+        # use mostly the same overlap calculation
+        # degree calculation should be handled in constructor
+
+def intersect_crosssyns(left, right):
