2fcac95f95b8fea84a766240276965879366162b
Leon Rauschning <leon.rauschning@tum.de>
2022-06-30 15:41:34 +0200

    deleted crossyn

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import numpy
 setup(ext_modules = cythonize([
             Extension('ingest', ['ingest.pyx'], include_dirs=[numpy.get_include()]),
             Extension('coresyn', ['coresyn.pyx']),
-            Extension('crosssyn', ['crosssyn.pyx']),
             Extension('syntools', ['syntools.pyx']),
             Extension('util', ['util.pyx']),
             Extension('cigar', ['cigar.pyx'])
