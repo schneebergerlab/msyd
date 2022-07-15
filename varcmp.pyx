8d17efe8cd61a58f285792c554dc1ad8ce349398
Leon Rauschning <leon.rauschning@tum.de>
2022-07-15 16:48:55 +0200

    initial implementation of get_neighbourhood

--- varcmp.pyx
+++ varcmp.pyx
@@ -10,7 +10,7 @@ import pandas as pd
 
 def get_snps(syri, rngs):
     """
-    
+    A function that finds all SNPs annotated within one of the input region, as a dictionary keyed by organism.
     :params:
         `syri`: dataframe containing the information from a syri file, as parsed by `ingest.readsyriout`
         `rngs`: a List of `Range` objects to extract snps from.
@@ -19,17 +19,51 @@ def get_snps(syri, rngs):
     """
     pass
 
+#TODO maybe refactor neighbourhoods out into a class to allow separate saving of overlap/ proper neighbours, comparison operators etc.
+# maybe optionally save with variant annotation?
+
 # idea from manish: check equality between SVs by neighbourhood pansyntenic regions
 def get_pansyn_neighbours(rng: Range, pansyns, ref=True, overlapping=True):
     """
     Given a `Range` and a list of pansyntenic regions (as `Pansyn`), returns the regions neighbouring or overlapping with that region on the organism specified in the `Range`.
     :returns: a list of `Pansyn` objects
     """
-    pass
+    # neighbouring := nearest in each direction
+    if len(pansyns) < 2:
+        print("WARN: get_pansyn_neighbours called with just one pansyn, returning as neighbours!")
+        return pansyns
+
+    org = rng.org # for now uses only reference, TODO properly use organism, maybe with refactor to dictionary in Pansyn?
+    ret = []
+    prev = None
+    paniter = iter(pansyns.ranges)
+    prev = next(paniter)
+    cur = next(paniter)
+    try:
+        while cur.ref.end < rng.start:
+            prev = cur
+            cur = next(paniter)
+
+        ret.append(prev) # left neighbour
+        del prev # no longer need to save previous values
+        while cur.ref.start < rng.end:
+            if overlapping:
+                ret.append(cur)
+            cur = next(paniter)
+
+        ret.append(cur) # right neighbour
+                
+    except StopIteration:
+        print("WARN: get_pansyn_neighbours found rng to be right of all pansyntenic regions! Double-Check if calling with the right set of pansyns")
+
+    return ret
+
+
 
 def cmp_neighbourhoods(l, r):
     """
     Given two neighbourhoods calculated by `get_pansyn_neighbours`, tries to determine if they are the same.
+    Use a function to be able to use more sophisticated comparisons.
     :returns: True/False, optionally maybe output distance value
     """
     pass
