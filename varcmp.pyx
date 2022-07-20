582eeb999ef4c43fd4cd301076159250c7578994
Leon Rauschning <leon.rauschning@tum.de>
2022-07-20 10:12:56 +0200

    refactored into Neighbourhood class

--- varcmp.pyx
+++ varcmp.pyx
@@ -19,53 +19,66 @@ def get_snps(syri, rngs):
     """
     pass
 
-#TODO maybe refactor neighbourhoods out into a class to allow separate saving of overlap/ proper neighbours, comparison operators etc.
-# maybe optionally save with variant annotation?
-
-# idea from manish: check equality between SVs by neighbourhood pansyntenic regions
-def get_pansyn_neighbours(rng: Range, pansyns, ref=True, overlapping=True):
+# maybe optionally save with variant annotation later?
+class Neighbourhood:
     """
-    Given a `Range` and a list of pansyntenic regions (as `Pansyn`), returns the regions neighbouring or overlapping with that region on the organism specified in the `Range`.
-    :returns: a list of `Pansyn` objects
+    A class that stores a genomic syntenic neighbourhood, defined by a left and right `Pansyn` neighbour.
+    Optionally also stores the syntenic regions overlapping the feature.
+    All of `left`, `right` and `overlapping` can be `None`; this means that the neighbourhood is at the leftmost/rightmost edge of a chromosome or that no overlap information is stored, respectively.
+    If overlap information is stored, but there are no overlaps, `[]` should be used instead.
     """
-    # neighbouring := nearest in each direction
-    if len(pansyns) < 2:
-        print("WARN: get_pansyn_neighbours called with just one pansyn, returning as neighbours!")
-        return pansyns
+    def __init__(self, left: Pansyn, right: Pansyn, overlapping):
+        self.left = left
+        self.right = right
+        self.overlapping = overlapping
 
-    org = rng.org
-    ret = []
-    prev = None
-    paniter = iter(pansyns.ranges)
-    prev = next(paniter)
-    cur = next(paniter)
-    try:
-        while cur.ref.end < rng.start if ref else cur.ranges_dict[org].end < rng.start: # maybe TODO refactor to use more elegant polymorphism than ternaries?
-            prev = cur
-            cur = next(paniter)
+    def __eq__(l, r):
+        # do not check for overlapping
+        return l.left == r.left and l.right == r.right
 
-        ret.append(prev) # left neighbour
-        del prev # no longer need to save previous values
-        
-        # get overlapping 'neighbours'
-        while cur.ref.start < rng.end if ref else cur.ranges_dict[org].start < rng.end:
-            if overlapping:
-                ret.append(cur)
-            cur = next(paniter)
+    def dist(l, r):
+        # TODO implement a distance function between two neighbourhood objects
+        # things to take into account:
+        #    – distances to left/right neighbour
+        #    – overlapping regions (?)
+        # maybe rewrite __eq__ to use dist with a threshold?
+        pass
 
-        ret.append(cur) # right neighbour
-                
-    except StopIteration:
-        print("WARN: get_pansyn_neighbours found rng to be right of all pansyntenic regions! Double-Check if calling with the right set of pansyns")
+    def get_pansyn_neighbours(rng: Range, pansyns, ref=True, overlapping=True):
+        """
+        Given a `Range` and a list of pansyntenic regions (as `Pansyn`), returns the regions neighbouring or overlapping with that region on the organism specified in the `Range`.
+        :returns: a `Neighbour` object
+        """
+        # neighbouring := nearest in each direction
+        if len(pansyns) < 2:
+            print("WARN: get_pansyn_neighbours called with just one pansyn, returning as neighbours!")
+            return pansyns
 
-    return ret
+        org = rng.org
+        prev = None
+        paniter = iter(pansyns.ranges)
+        prev = next(paniter)
+        cur = next(paniter)
+        left = None
+        right = None
+        overlapping = [] if overlapping else None
+        try:
+            while cur.ref.end < rng.start if ref else cur.ranges_dict[org].end < rng.start: # maybe TODO refactor to use more elegant polymorphism than ternaries?
+                prev = cur
+                cur = next(paniter)
 
+            left = prev
+            del prev # no longer need to save previous values
+            
+            # get overlapping 'neighbours'
+            while cur.ref.start < rng.end if ref else cur.ranges_dict[org].start < rng.end:
+                if overlapping:
+                    overlapping.append(cur)
+                cur = next(paniter)
 
+            right = cur
+                    
+        except StopIteration:
+            print("WARN: get_pansyn_neighbours found rng to be right of all pansyntenic regions! Double-Check if calling with the right set of pansyns")
 
-def cmp_neighbourhoods(l, r):
-    """
-    Given two neighbourhoods calculated by `get_pansyn_neighbours`, tries to determine if they are the same.
-    Use a function to be able to use more sophisticated comparisons.
-    :returns: True/False, optionally maybe output distance value
-    """
-    pass
+        return Neighbourhood(left, right, overlapping)
