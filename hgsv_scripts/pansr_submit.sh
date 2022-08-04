928734d12d17d5718914562bd902a037f7ad8542
mnshgl0110 <mnshgl0110@gmail.com>
2022-08-04 11:28:26 +0200

    adding test

--- hgsv_scripts/pansr_submit.sh
+++ hgsv_scripts/pansr_submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bsub -q normal -n4 -R"span[hosts=1] rusage[mem=64000]" -M1280000 \
--oo job_all.log -eo job_all.err \
-"python ../../pansr/main.py all.tsv 4"
+-oo job_$1.log -eo job_$1.err \
+"python ../../pansr/main.py $1 4"
