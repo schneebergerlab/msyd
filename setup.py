5b36ec915ff655ce9db737239f505848c0e3b0b6
Leon Rauschning <leon.rauschning@tum.de>
2022-08-05 15:30:39 +0200

    cleanup, now setup is fairly pretty i think (s. https://github.com/schneebergerlab/pansr/issues/2\#issuecomment-1206386960)

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-ingest_ext = Extension('pansr.ingest', ['ingest.pyx'], include_dirs=[numpy.get_include()])
-pyx_ext = Extension('*', ['*.pyx'])
 setup(
         ext_modules = cythonize('pansr/*.pyx'),#[ingest_ext, pyx_ext]),
         scripts=["main.py"],
