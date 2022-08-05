8d23a61165488bf8366cd9caecb3053b5d5c731a
Leon Rauschning <leon.rauschning@tum.de>
2022-08-05 12:02:13 +0200

    Reworked setup.py to use globs Still have to explicitly define `ingest` due to https://github.com/cython/cython/issues/1480

--- setup.py
+++ setup.py
@@ -3,13 +3,9 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules = cythonize([
-            Extension('ingest', ['ingest.pyx'], include_dirs=[numpy.get_include()]),
-            Extension('pansyn', ['pansyn.pyx']),
-            Extension('varcmp', ['varcmp.pyx']),
-            Extension('util', ['util.pyx']),
-            Extension('cigar', ['cigar.pyx']),
-            Extension('classes', ['classes.pyx'])
-            ]),
+ingest_ext = Extension('ingest', ['ingest.pyx'], include_dirs=[numpy.get_include()])
+pyx_ext = Extension('*', ['*.pyx'])
+setup(
+        ext_modules = cythonize([ingest_ext, pyx_ext]),
         scripts=["main.py"]
         )
