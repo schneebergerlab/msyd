939804877d30a7ac32f30bf249e7f5dd58a69115
Leon Rauschning <leon.rauschning@tum.de>
2022-08-08 20:25:14 +0200

    order matched to pansyri order

--- ampril_scripts/plotsr_call.sh
+++ ampril_scripts/plotsr_call.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-plotsr --genomes genomes.txt -o ../plotsr.png --sr an1_c24syri.out --sr c24_colsyri.out --sr col_cvisyri.out --sr cvi_erisyri.out --sr eri_kyosyri.out --sr kyo_lersyri.out --sr ler_shasyri.out
+plotsr --genomes genomes_lex.txt -o ~/plotsr_lex.png\
+	--sr an1_c24syri.out\
+	--sr c24_colsyri.out\
+	--sr col_cvisyri.out\
+	--sr cvi_erisyri.out\
+	--sr eri_kyosyri.out\
+	--sr kyo_lersyri.out\
+	--sr ler_shasyri.out
+
+plotsr --genomes genomes_lex.txt -o ~/plotsr_pansyri.png\
+	--sr kyo_cvisyri.out\
+	--sr cvi_an1syri.out\
+	--sr an1_colsyri.out\
+	--sr col_lersyri.out\
+	--sr ler_c24syri.out\
+	--sr c24_erisyri.out\
+	--sr eri_shasyri.out
