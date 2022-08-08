321b4f054e0da847bd1e9008388c38dcbda641a7
Leon Rauschning <leon.rauschning@tum.de>
2022-08-08 20:03:50 +0200

    plotsr_call added

---
+++ ampril_scripts/plotsr_call.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+plotsr --genomes genomes.txt -o ../plotsr.png --sr an1_c24syri.out --sr c24_colsyri.out --sr col_cvisyri.out --sr cvi_erisyri.out --sr eri_kyosyri.out --sr kyo_lersyri.out --sr ler_shasyri.out
