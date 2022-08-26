a7b5a837e34dbffbe42748015a192d62de117f9f
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2022-08-26 16:28:50 +0200

    more tests

--- tests/test_cigar.py
+++ tests/test_cigar.py
@@ -5,9 +5,19 @@ import pansyri.classes.cigar as cigar
 from pansyri.classes.cigar import Cigar
 import pytest
 
-@pytest.fixture(params=["10=", "10=2X4=", "100=20I4X5D10I40=", "10=30D5=2X2=20I10="])
-def example_cigar():
-    return Cigar.from_string("10=")
+@pytest.fixture(params=["100=", "10=2X4=", "100=20I4X5D10I40=", "10=30D5=2X2=20I10="])
+def example_cigar(request):
+    return Cigar.from_string(request.param)
+
+# cigars guaranteed to have more than 100 bp on both ref and qry
+@pytest.fixture(params=["100=", "10=2X40=1X20=1X25=2X10=", "100=20I3=4X4=5D10I40=", "10=30D5=4X2=20I10="])
+def example_long_cigar(request):
+    return Cigar.from_string(request.param)
+
+# cigars guaranteed to have fewer than 10 bp on both ref and qry
+@pytest.fixture(params=["3=1X5=", "9=", "3=2I4D2=", "3D2=1X2="])
+def example_short_cigar(request):
+    return Cigar.from_string(request.param)
 
 def test_from_strings():
     tuplelists = [ [ [8, '='], [2, 'I'], [4, '='], [2, 'D']], [[100, '=']], [[100, '='], [2, 'X'], [20, '='], [4, 'I'], [40, '='], [1, 'X']] ]
@@ -38,5 +48,38 @@ def test_padding(example_cigar, l, r):
     example_cigar.unpad()
     assert example_cigar.get_len(ref=True) == lref
     assert example_cigar.get_len(ref=False) == lqry
+
+def test_unequal(example_cigar, example_short_cigar):
+    assert not example_cigar == example_short_cigar
+    assert example_cigar != example_short_cigar
+
+def test_equal(example_cigar):
+    assert example_cigar == example_cigar
+    assert not example_cigar != example_cigar
+
+
+
+selectively_roll_cigar = lambda x, y: (x, Cigar.from_string(y))
+def test_getrem():
+    # always skip 10 from end/start
+    skip = 10
+    cgs = [Cigar.from_string(x) for x in ["100=", "5D10=1X10=5I", "50=2I1X4D3="]]
+    startrems = [selectively_roll_cigar(*x) for x in [(10, "90="), (5, "5=1X10=5I"), (10, "40=2I1X4D3=")]]
+    endrems = [selectively_roll_cigar(*x) for x in [(10, "90="), (15, "5D10=1X"), (8, "48=")]]
+
+    for cg, startrem, endrem in zip(cgs, startrems, endrems):
+        assert startrem == cg.get_removed(skip)
+        assert endrem == cg.get_removed(skip, start=False)
+
+@pytest.mark.parametrize("ref", [True, False])
+def test_getrem_inv(example_cigar, ref):
+    # tests that reversing a cigar string and then skipping X is the same as getting X 
+    cg_rev = Cigar(example_cigar.pairs[::-1])
+
+    invskip, invrem = cg_rev.get_removed(10, ref=ref)
+    skip, rem = example_cigar.get_removed(10, ref=ref, start=False)
+    assert invskip == skip
+    assert invrem.pairs[::-1] == rem.pairs
+
 
 
