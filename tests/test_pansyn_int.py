c3f3e9b7c58d7b0b197dbfce039a296ae044d754
Leon Rauschning <leon.rauschning@tum.de>
2022-10-10 18:12:21 +0200

    .

--- tests/test_pansyn_int.py
+++ tests/test_pansyn_int.py
@@ -33,7 +33,7 @@ def read_fasta(f):
                     strbuf = ''
                 key = line[1:]
             else:
-                strbuf += line
+                strbuf += line.strip()
 
     return ret
 
@@ -50,6 +50,10 @@ def test_pansyn_int():
     genome_files = [aln.split('.')[0].split('_')[-1] + '.filtered.fa.gz' for aln in alns]
     gens = {f.split('.')[0]:read_fasta(f) for f in genome_files} # key by org name
     refgen = read_fasta('./col.filtered.fa.gz')
+    print([x for x in refgen.keys])
+    print([x for x in gens.keys])
+    for gen in gens:
+        print(x for x in gen.keys)
     
     # get pansyn df
     df = util.crosssyn_from_lists(syns, alns)
