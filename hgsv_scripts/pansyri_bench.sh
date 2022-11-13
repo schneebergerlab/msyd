a2a81c61a3dcbc44ec371f046ebe9fc357efdf62
Leon Rauschning <leon.rauschning@tum.de>
2022-11-13 18:38:57 +0100

    increased memory limits, should avoid swapping on busy cluster

--- hgsv_scripts/pansyri_bench.sh
+++ hgsv_scripts/pansyri_bench.sh
@@ -2,7 +2,7 @@
 
 for n in $(seq 1 20)
 do
-	bsub -q multicore20 -n20 -R"span[hosts=1] rusage[mem=64000]" -M1280000 \
-	-oo bench_$n.log -eo bench_$n.err \
-	"pansyri -i ./all.tsv -c 20 --discard"
+	bsub -q multicore20 -n10 -R"span[hosts=1] rusage[mem=128000]" -M1280000 \
+	-oo bench_new_$n.log -eo bench_new_$n.err \
+	"pansyri -i ./haplos.tsv -c 10 --discard"
 done
--- hgsv_scripts/pansyri_benchp.sh
+++ hgsv_scripts/pansyri_benchp.sh
@@ -5,8 +5,8 @@ for c in $(seq 20 4 40)
 do
 	for n in $(seq 1 5)
 	do
-		bsub -q multicore40 -n$c -R"span[hosts=1] rusage[mem=64000]" -M1280000 \
+		bsub -q multicore40 -n$c -R"span[hosts=1] rusage[mem=128000]" -M1280000 \
 		-oo benchp_$c_$n.log -eo benchp_$c_$n.err \
-		"pansyri -i ./all.tsv -c $c --discard"
+		"pansyri -i ./haplos.tsv -c $c --discard"
 	done
 done
