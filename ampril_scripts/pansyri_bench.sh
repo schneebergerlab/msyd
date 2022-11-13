7f28540a7511d45ade9e2ee73615df57a7cb6ab7
Leon Rauschning <leon.rauschning@tum.de>
2022-11-13 19:03:33 +0100

    also did for ampril dataset

--- ampril_scripts/pansyri_bench.sh
+++ ampril_scripts/pansyri_bench.sh
@@ -2,7 +2,7 @@
 
 for n in $(seq 1 20)
 do
-	bsub -q multicore20 -n8 -R"span[hosts=1] rusage[mem=8000]" -M320000 \
-	-oo bench_$n.log -eo bench_$n.err \
-	"pansyri -i ./full.tsv -c 8 --discard"
+	bsub -q short -n4 -R"span[hosts=1] rusage[mem=20000]" -M320000 \
+	-oo bench_new_$n.log -eo bench_new_$n.err \
+	"time pansyri -i ./full.tsv -c 4 --discard"
 done
