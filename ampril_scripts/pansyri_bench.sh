325499cb3d92545cf32f3cff2bbc9c346ccdb0c2
Leon Rauschning <leon.rauschning@tum.de>
2022-11-13 19:08:34 +0100

    redid to multicore

--- ampril_scripts/pansyri_bench.sh
+++ ampril_scripts/pansyri_bench.sh
@@ -2,7 +2,7 @@
 
 for n in $(seq 1 20)
 do
-	bsub -q short -n4 -R"span[hosts=1] rusage[mem=20000]" -M320000 \
+	bsub -q multicore20 -n4 -R"span[hosts=1] rusage[mem=20000]" -M320000 \
 	-oo bench_new_$n.log -eo bench_new_$n.err \
 	"time pansyri -i ./full.tsv -c 4 --discard"
 done
