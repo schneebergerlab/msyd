26f774fb5aaa8aa465419aad39a09f89ecf9768f
Leon Rauschning <leon.rauschning@tum.de>
2022-11-14 11:04:36 +0100

    changed to short queue

--- ampsynth_scripts/pansyri_benchmark.sh
+++ ampsynth_scripts/pansyri_benchmark.sh
@@ -2,7 +2,7 @@
 
 for n in $(seq 1 20)
 do
-	bsub -q multicore20 -n4 -R"span[hosts=1] rusage[mem=5000]" -M80000 \
-	-oo job_$n.log -eo job_$n.err \
-	"pansyri -i ./all.tsv -c 4 --discard"
+	bsub -q short -n1 -R"span[hosts=1] rusage[mem=5000]" -M80000 \
+	-oo bench_short_$n.log -eo bench_short_$n.err \
+	"pansyri -i ./all.tsv -c 1 --discard"
 done
