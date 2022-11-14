5dea941fbcda00e480cc1b6009e76b693c2ddc1c
Leon Rauschning <leon.rauschning@tum.de>
2022-11-14 11:47:40 +0100

    added time

--- ampsynth_scripts/pansyri_benchmark.sh
+++ ampsynth_scripts/pansyri_benchmark.sh
@@ -4,5 +4,5 @@ for n in $(seq 1 20)
 do
 	bsub -q short -n1 -R"span[hosts=1] rusage[mem=5000]" -M80000 \
 	-oo bench_short_$n.log -eo bench_short_$n.err \
-	"pansyri -i ./all.tsv -c 1 --discard"
+	"time pansyri -i ./all.tsv -c 1 --discard"
 done
