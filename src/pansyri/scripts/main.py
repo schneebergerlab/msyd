946d5bd93865d14fd06890e79476f11ea67a0245
Leon Rauschning <leon.rauschning@tum.de>
2023-01-16 18:34:24 +0100

    redid cli interface

--- src/pansyri/scripts/main.py
+++ src/pansyri/scripts/main.py
@@ -25,52 +25,59 @@ This file serves as the main entrypoint for the pansyri CLI.
 
 def main(argv):
 
-    #if argv[0] == 'impute':
-    #    print(imputation.impute_strings(argv[1], argv[2]))
-    #    sys.exit()
-
-    ## Argument parsing redo
-    # Idea:
-    # – different modes, selectablel by first positional argument
-    # – call, filter, statistics, order, lengths mode?
-    # – have subparsers for each, mb share common arguments like -c and -i/-o
-
-
     parser = argparse.ArgumentParser(description="Pansyri is a pansynteny and rearrangement identifier.")
-    parser.set_defaults(func=None)
-    # core arguments
-    parser.add_argument("-c", dest="cores", help="Number of cores to use for parallel computation. Defaults to 4.", type=int, default=4)
-    #parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="The .tsv file to read SyRI output and alignment files in from. For more details, see the Readme.")
-    parser.add_argument("-o", dest='outfile', default='-', type=argparse.FileType('wt'), help="Where to store disk output. Defaults to stdout (specified with \"-\").")
+    parser.set_defaults(func=None, cores=1)
 
     subparsers = parser.add_subparsers(description="See also pansyri [subparser] -h:") # title/description?
-    call_parser = subparsers.add_parser("call", description="Call Pansynteny and write to disk in VCF or PFF format.")
-    call_parser.set_defaults(func=call)
+    # ordering parser
     order_parser = subparsers.add_parser("order", description="Determine the optimal ordering of the supplied genomes for plotting.")
     order_parser.set_defaults(func=order)
-    filter_parser = subparsers.add_parser("filter", description="")
-    filter_parser.set_defaults(func=filter)
-    combinations_parser = subparsers.add_parser("combinations", description="Evaluate all combinations of --syn and --core in the same manner as the --stats parameter.")
-    combinations_parser.set_defaults(func=combinations)
+    order_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF file to read pansynteny information from.")
 
-    statistics_parser = subparsers.add_parser("statistics", description="Print some statistics of the called pansynteny.")
-    statistics_parser.set_defaults(func=statistics)
+    #combinations_parser = subparsers.add_parser("combinations", description="Evaluate all combinations of --syn and --core in the same manner as the --stats parameter.")
+    #combinations_parser.set_defaults(func=combinations)
+    #combinations_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF file to read pansynteny information from.")
+
+    # stats and plotting subparsers
+    stats_parser = subparsers.add_parser("stats", description="Print some statistics of the called pansynteny.")
+    stats_parser.set_defaults(func=stats)
+    stats_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF file to read pansynteny information from.")
     plot_parser = subparsers.add_parser("plot", description="Prints a lengths df for plotting to stdout. Can be piped to a file and plotted with tests/plot.R .")
     plot_parser.set_defaults(func=plot)
+    plot_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF or VCF file to read pansynteny information from.")
 
-
+    # Pansyn calling argparser
+    call_parser = subparsers.add_parser("call", description="Call Pansynteny and write to disk in VCF or PFF format.")
+    call_parser.set_defaults(func=call)
+    call_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="The .tsv file to read SyRI output and alignment files in from. For more details, see the Readme.")
+    call_parser.add_argument("-o", dest='outfile', default='-', type=argparse.FileType('wt'), help="Where to store disk output. Defaults to stdout (specified with \"-\").")
+    call_parser.add_argument("-c", dest="cores", help="Number of cores to use for parallel computation. Defaults to 4.", type=int, default=4)
     call_parser.add_argument("--core", dest='core', action='store_const', const=True, default=False, help="Call only core synteny. Improves runtime significantly, particularly on larger datasets.")
     call_parser.add_argument("--syn", "-s", dest='SYNAL', action='store_const', const=False, default=True, help="Use SYN instead of SYNAL regions, yields more contiguous regions and faster runtime, but calls may not be exact to the base level.")
     call_parser.add_argument("--no-cigars", dest='cigars', action='store_const', const=False, default=True, help="Don't store CIGAR strings in the saved .pff file. Has no effect when --syn is specified")
-    call_parser.add_argument("-v", "--vcf", dest='vcf', action='store_true', default=False, help="Store as VCF instead of PFF. Discards CIGAR strings!")
+    call_parser.add_argument("-v", "--vcf", dest='vcf', action='store_true', default=False, help="Export to CF instead of saving to PFF. PansyRI cannot (currently) read in pansynteny information that is not in PFF format!")
 
-    statistics_parser.add_argument("--vcf", dest='invcf', required=False, type=argparse.FileType('r'), help="The .vcf file to filter and write to -o. Only necessary when calling --filter-vcf.")
+    # Conversion parser
+    convert_parser = subparsers.add_parser("convert", description="Convert between different pansynteny information file formats")
+    convert_parser.set_defaults(func=convert)
+    convert_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF file to read pansynteny information from.")
+    convert_parser.add_argument("-t", dest='filetype', required=False, type=str, help="File format to convert to.")
+    convert_parser.add_argument("-o", dest='outfile', default='-', type=argparse.FileType('wt'), help="Where to store disk output. Defaults to stdout (specified with \"-\").")
+
+
+
+    # VCF filtering subparser
+    filter_parser = subparsers.add_parser("filter", description="")
+    filter_parser.set_defaults(func=filter)
+    filter_parser.add_argument("--vcf", dest='invcf', required=True, type=argparse.FileType('r'), help="The .vcf file to filter and write to -o.")
+    filter_parser.add_argument("-i", dest='infile', required=True, type=argparse.FileType('r'), help="PFF file to read pansynteny information from.")
+    filter_parser.add_argument("-o", dest='outfile', required=True, type=argparse.FileType('wt'), help="Where to store the filtered VCF file. Defaults to stdout (specified with \"-\").")
 
     args = parser.parse_args(argv)
     print(args)
     args.call(args)
 
-def call(args)
+def call(args):
     syns, alns = util.parse_input_tsv(args.infile)
     df = pansyn.find_multisyn(syns, alns, only_core=args.core, SYNAL=args.SYNAL)
     if args.vcf:
@@ -79,7 +86,7 @@ def call(args)
         io.save_to_pff(df, args.outfile, save_cigars=args.cigars)
 
 # call the plotsr ordering functionality on a set of organisms described in the .tsv
-def order(args): # TODO have subparsing for other ordering/scores/etc
+def order(args):
     df = io.read_pff(args.infile)
     print(ordering.order_hierarchical(df, orgs=None, score_fn=ordering.syn_score))
 
@@ -97,6 +104,18 @@ def combinations(args):
 def lengths(args):
     syns, alns = util.parse_input_tsv(args.infile)
     util.length_compare(syns, alns, cores=args.cores)
+
+def convert(args):
+    df = io.read_pff(args.infile)
+    # mb refactor into common output function
+    if not args.filetype:
+        args.filetype = args.outfile.name.split(".")[-1]
+    if args.filetype not in {'vcf', 'pff'}: # add bcf
+        logger.error("invalid output file format selected!")
+    if args.filetype == 'pff':
+        io.save_to_pff(df, args.outfile)
+    else:
+        io.save_to_vcf(df, args.outfile, cores=args.cores)
 
 def filter_vcf(args):
     df = io.read_pff(args.infile)
