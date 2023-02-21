10959220df4ee600fb0aabbcaaba212709b3aafc
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2023-02-21 18:42:03 +0100

    specified paths w/o home dir for cluster

--- tests/benchmark_ampril.sh
+++ tests/benchmark_ampril.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # small script that 
-cd /home/lrauschning/pansyri/pansyri
+cd /netscratch/dep_mercier/grp_schneeberger/projects/pansr/pansyri
 #git pull && python setup.py install
 curtime=$(date --rfc-3339=seconds)
 commit=$(git show --oneline -s) 
 cores=4
-cd /home/lrauschning/pansyri/data/ampril
+cd /netscratch/dep_mercier/grp_schneeberger/projects/pansr/data/ampril
 
 rawout=''
 for x in seq 1 10
@@ -18,4 +18,4 @@ avgs=$(echo "rawout='$rawout'.split(';')[:-1];print('time:', ' s, mem: '.join([s
 # in case of not doing avgs:
 #avgs=$(/usr/bin/time -f "time: %e s, mem: %M Kb" pansyri call -i full.tsv -v /dev/null -c $cores 2>&1 > /dev/null | tail -n 1)
 
-echo $curtime – $commit: $avgs on $cores cores at $(hostname) >> /home/lrauschning/pansyri/pansyri/benchmarks.txt
+echo $curtime – $commit: $avgs on $cores cores at $(hostname) >> /netscratch/dep_mercier/grp_schneeberger/projects/pansr/pansyri/benchmarks.txt
