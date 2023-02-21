96863f0f53e522106ae2458680ff1aba226a8f21
Leon Rauschning <l.rauschning@mpipz.mpg.de>
2023-02-21 17:43:06 +0100

    fixed typo

--- tests/benchmark_hgsv.sh
+++ tests/benchmark_hgsv.sh
@@ -10,12 +10,12 @@ cd /home/lrauschning/pansyri/data/hgsv
 #rawout=''
 #for x in seq 1 10
 #do
-#	rawout=$rawout$(/usr/bin/time -f "%e,%m;" pansyri call -i full.tsv -v /dev/null -c $cores 2>&1 > /dev/null | tail -n 1)
+#	rawout=$rawout$(/usr/bin/time -f "%e,%M;" pansyri call -i full.tsv -v /dev/null -c $cores 2>&1 > /dev/null | tail -n 1)
 #done
 #echo $rawout
 #avgs=$(echo "rawout='$rawout'.split(';')[:-1];print('time:', ' s, mem: '.join([str(sum([float(tup.split(',')[ind]) for tup in rawout])/len(rawout)) for ind in [0, 1]]), 'Kb')" | python) # it's not ugly if it works!
 
 # in case of not doing avgs:
-avgs=$(/usr/bin/time -f "time: %e s, mem: %K Kb" pansyri call -i all.tsv -v /dev/null -c $cores 2>&1 > /dev/null | tail -n 1)
+avgs=$(/usr/bin/time -f "time: %e s, mem: %M Kb" pansyri call -i all.tsv -v /dev/null -c $cores 2>&1 > /dev/null | tail -n 1)
 
 echo $curtime – $commit: $avgs on $cores cores at $(hostname) >> /home/lrauschning/pansyri/pansyri/benchmarks.txt
