4f27e6575216a2d230e70aa0cf27097185034881
Leon Rauschning <leon.rauschning@tum.de>
2023-03-15 11:49:21 +0100

    fixed an1 name

--- ampril_scripts/run_syri.sh
+++ ampril_scripts/run_syri.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for ref in an24 col cvi eri kyo ler sha; do
+for ref in an1 col cvi eri kyo ler sha; do
 	for sample in an24 col cvi eri kyo ler sha; do
 		echo doing $ref\_$sample
 		syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
