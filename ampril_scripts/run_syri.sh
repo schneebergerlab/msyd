847a73a3f01ae8c3f84a25167b84397c643b3ffd
Leon Rauschning <leon.rauschning@tum.de>
2023-03-15 11:48:25 +0100

    escaped _s

--- ampril_scripts/run_syri.sh
+++ ampril_scripts/run_syri.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 for ref in an24 col cvi eri kyo ler sha; do
-	echo doing $ref
 	for sample in an24 col cvi eri kyo ler sha; do
-		syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref_$sample -c $ref_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref_$samplesyri.log --samplename $sample
+		echo doing $ref\_$sample
+		syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
 	done
 done
 
