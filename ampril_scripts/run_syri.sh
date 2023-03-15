2bc33e293edf47d7cbbbad5c75f673872bffc5f8
Leon Rauschning <leon.rauschning@tum.de>
2023-03-15 11:54:36 +0100

    now not doing ref_ref

--- ampril_scripts/run_syri.sh
+++ ampril_scripts/run_syri.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 for ref in an1 col cvi eri kyo ler sha; do
 	for sample in an1 col cvi eri kyo ler sha; do
-		echo doing $ref\_$sample
-		syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
+		if [[ $ref != $sample ]]; then
+			echo doing $ref\_$sample
+			syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
+		fi
 	done
 done
 
