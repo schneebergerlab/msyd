03e293b13950c96888bc65f74d0f31f9639cca9c
Leon Rauschning <leon.rauschning@tum.de>
2023-04-03 18:23:56 +0200

    added c24 to the script

--- ampril_scripts/run_syri.sh
+++ ampril_scripts/run_syri.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for ref in an1 col cvi eri kyo ler sha; do
-	for sample in an1 col cvi eri kyo ler sha; do
+for ref in an1 c24 col cvi eri kyo ler sha; do
+	for sample in an1 c24 col cvi eri kyo ler sha; do
 		if [[ $ref != $sample ]]; then
 			echo doing $ref\_$sample
 			syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
