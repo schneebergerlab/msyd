f453eb2b4eb042b3872a62ff2100396d9c90a342
Leon Rauschning <leon.rauschning@tum.de>
2023-04-03 18:29:58 +0200

    updated ampril scripts

--- ampril_scripts/pansyn_bench.sh
+++ ampril_scripts/pansyn_bench.sh
@@ -4,5 +4,5 @@ for n in $(seq 1 20)
 do
 	bsub -q multicore20 -n4 -R"span[hosts=1] rusage[mem=20000]" -M320000 \
 	-oo bench_new_$n.log -eo bench_new_$n.err \
-	"time pansyri -i ./full.tsv -c 4 --discard"
+	"time pansyn call -i ./full.tsv -c 4 -o /dev/null"
 done
--- ampril_scripts/run_syri.sh
+++ ampril_scripts/run_syri.sh
@@ -4,7 +4,7 @@ for ref in an1 c24 col cvi eri kyo ler sha; do
 	for sample in an1 c24 col cvi eri kyo ler sha; do
 		if [[ $ref != $sample ]]; then
 			echo doing $ref\_$sample
-			syri --nc 5 -F B --cigar --dir ~/pansyri/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
+			syri --nc 5 -F B --cigar --dir ~/pansyn/data/ampril --prefix $ref\_$sample -c $ref\_$sample.bam -r $ref.filtered.fa.gz -q $sample.filtered.fa.gz --lf $ref\_$samplesyri.log --samplename $sample
 		fi
 	done
 done
