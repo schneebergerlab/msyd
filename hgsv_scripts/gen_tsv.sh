bc31d23f16513289d6925b497263be52d5f62e23
Leon Rauschning <leon.rauschning@tum.de>
2023-05-12 10:59:02 +0200

    fix duplicate names

--- hgsv_scripts/gen_tsv.sh
+++ hgsv_scripts/gen_tsv.sh
@@ -5,6 +5,6 @@ for f in syri/*syri.out
 do
 	bs=$(basename $f syri.out)
 	# shorten the name even further to make it readable
-	name="$(echo $bs | grep -o -P '(NA|HG)\d+').$(echo $bs | grep -o -P 'h[1|0]')"
+	name="$(echo $bs | grep -o -P '(NA|HG)\d+').$(echo $bs | grep -o -P 'h[2|1|0]').$(echo $bs | grep -o -P 'flye')"
 	echo "$name	alns_filtered/$bs.paf	syri/${bs}syri.out	syri/${bs}syri.vcf" >> all.tsv
 done
