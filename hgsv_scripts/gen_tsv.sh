39cfa34b6750e27e773682b157ae7bc1c2ea492a
Leon Rauschning <leon.rauschning@tum.de>
2023-07-18 11:24:41 +0200

    hgsv added sequence

--- hgsv_scripts/gen_tsv.sh
+++ hgsv_scripts/gen_tsv.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "#name aln 	syri	vcf" > all.tsv
+echo "#name aln 	syri	vcf	sequence" > all.tsv
 for f in syri/*syri.out
 do
 	bs=$(basename $f syri.out)
 	# shorten the name even further to make it readable
 	name="$(echo $bs | grep -o -P '(NA|HG)\d+').$(echo $bs | grep -o -P 'h[2|1|0]').$(echo $bs | grep -o -P 'flye')"
-	echo "$name	alns_filtered/$bs.paf	syri/${bs}syri.out	syri/${bs}syri.vcf" >> all.tsv
+	echo "$name	alns_filtered/$bs.paf	syri/${bs}syri.out	syri/${bs}syri.vcf	scaffolds_filtered/$bs" >> all.tsv
 done
