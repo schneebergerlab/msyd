a9d2d2b712dfc4e11eee4eedc77a2b14e2e711be
Manish Goel <mnshgl0110@gmail.com>
2024-02-23 17:16:45 +0100

    increased memory helped crashing jobs

--- sh/sbatch/run_syri_on_potato.sh
+++ sh/sbatch/run_syri_on_potato.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --array=0-9
+#SBATCH --array=0-10
 #SBATCH --get-user-env
 #SBATCH --clusters=biohpc_gen
 #SBATCH --partition=biohpc_gen_normal
-#SBATCH --ntasks=4
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=2000mb
+#SBATCH --mem-per-cpu=5000mb
 #SBATCH --time=12:00:00
 #SBATCH -J QUickScript
 
-module load samtools
+#module load samtools
+
 chars=({A..J})
 for i in 5 6 7 8; do
 #  srun minimap2 -ax asm5 -t 10 --eqx DM_chr02.fa ${chars[${SLURM_ARRAY_TASK_ID}]}_chr02_hap${i}.fa \
@@ -17,7 +18,7 @@ for i in 5 6 7 8; do
 #  srun samtools
 	echo ${SLURM_CPUS_PER_TASK}
 	echo ${chars[${SLURM_ARRAY_TASK_ID}]}
-	srun --exclusive --ntasks=1 --cpus-per-task=${SLURM_CPUS_PER_TASK} --mem-per-cpu=2000 /dss/dsslegfs01/pn29fi/pn29fi-dss-0003/software/bin_manish/anaconda3/envs/mgpy3.8/bin/hometools runsyri -alignment bam \
+	srun --exclusive --ntasks=1 --cpus-per-task=${SLURM_CPUS_PER_TASK} --mem-per-cpu=5000 /dss/dsslegfs01/pn29fi/pn29fi-dss-0003/software/bin_manish/anaconda3/envs/mgpy3.8/bin/hometools runsyri -alignment bam \
 	-n 10 -p dm_${chars[${SLURM_ARRAY_TASK_ID}]}_chr02_hap${i} \
 	DM_chr02.fa ${chars[${SLURM_ARRAY_TASK_ID}]}_chr02_hap${i}.fa &
 #srun --ntasks 1 --cpus-per-task ${SLURM_CPUS_PER_TASK} --mem-per-cpu=1000 /dss/dsslegfs01/pn29fi/pn29fi-dss-0003/software/bin_manish/anaconda3/envs/mgpy3.8/bin/hometools runsyri -alignment bam -n 10 -p chr02_dm_A_hap${i} DM_chr02.fa chr02_hap${i}.fa
