efccd187113bf5439fda0c27cddb57398ab77a2b
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-11-12 19:03:50 +0800

    fully implement stuff in private

--- msyd/pyxfiles/private.pyx
+++ msyd/pyxfiles/private.pyx
@@ -12,27 +12,39 @@ from collections import deque, defaultdict
 from multiprocessing import Pool
 
 import msyd.intersection as intersection
+import msyd.util as util
 from msyd.multisyn import Multisyn, Private
 from msyd.coords import Range
 
-cdef MIN_PRIV_THRESH = intersection.MIN_SYN_THRESH
+cdef int MIN_PRIV_THRESH = intersection.get_min_syn_thresh()
 
 
 logger = util.CustomFormatter.getlogger(__name__)
 logger.setLevel(logging.INFO)
 
-cpdef complement_dict(syndict):
-    """
-    Interface to call get_privates on a dict containing multiple multisyn dfs.
-    """
-    #TODO implement
-    pass
 
-cdef complement(multisyns):
+#THOUGHT implement a higher-order function for handling these kinds of things in utils?
+cpdef complement_dict(syndict, add=False, cores=1):
+    """
+    Interface to call complement on a dict containing multiple multisyn DFs.
+    :returns: A dict mapping chromosomes to DFs containing the private and/or Multisyn objects on them.
+    """
+    if cores > 1:
+        with Pool(cores) as pool:
+            return dict(pool.map(_workaround, [(it[0], it[1], add) for it in syndict.items()]))
+    else:
+        return dict(map(_workaround, [(it[0], it[1], add) for it in syndict.items()]))
+
+cpdef _workaround(tup): # tup: [chrom, msyn, add]
+    # Annoying workaround, because multiprocessing doesn't like lambdas
+    return tup[0], complement(tup[1], add=tup[2])
+
+cdef complement(multisyns, add=False):
     """
     Takes a DF of multisyn objects, finds regions on the reference not covered by any of them.
     Assumes the multisyns are sorted by position on the reference.
     This can be thought of as a way to compute the complement of the syntenic regions.
+
     :returns: a DF containing Private objects, sorted by position on reference
     """
     #THOUGHT: should private inherit from multisyn?
@@ -43,36 +55,34 @@ cdef complement(multisyns):
         ret = deque()
     
     for _, msyn in multisyns.iterrows():
+        msyn = msyn[0]
+
+        # compute gap between previous msyn and this, annotate if large enough
         rng = msyn.ref
         if rng.start - cov >= MIN_PRIV_THRESH:
             ret.append(Private(Range(rng.org, rng.chr, cov, rng.start - 1)))
         cov = rng.end
+        
+        # if specified, also add the msyn while maintaining sorting
+        if add:
+            ret.append(msyn)
 
     # will ignore private regions at the end of the chromosome;
-    # probably makes sense, though
+    # probably hard to explicitly annotate them due to unknown telomere length anyway
 
-    return pd.DataFrame(ret)
-
+    return pd.DataFrame(data=list(ret))
 
 cdef intersect_private(l, r):
     """
     Takes two DataFrames of private regions on the same reference, returns a DF containing regions private in both.
     """
-    return find_overlaps(l, r, only_core=True, trim=False, allow_private=True)
+    return intersection.find_overlaps(l, r, only_core=True, trim=False, allow_private=True)
 
-
-
-
-#TODO integrate this into main.py/util.py
 #TODO debug/validate
 #TODO make complement work with realignment datastructure
 # => will require accounting for mappingtree/other multisyns 
 # => do this on the synthetic sequences, before mapping back to real sequence space?
 # => spacers might be a problem
-
-
-
-
 
 
 # Idea for additional fn
