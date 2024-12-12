3082802a77743445fd05614159fc46ad92e077c3
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-12-12 14:04:30 +0800

    refactor example_worklfow test run into separate script to preserve alias

---
+++ .github/workflows/run_test_example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# alias to a call launching the syri entrypoint from python
+# necessary, as the hacky git install does not install the CLI entrypoints
+alias syri='python <(echo "import syri.scripts.syri;syri.scripts.syri.main()")'
+
+# run using source to preserve alias
+source ../../example/example_workflow.sh
