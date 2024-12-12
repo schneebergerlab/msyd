0736cb4730c26892b3193f57e6a85c57f4622bf7
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-12-12 14:18:57 +0800

    change path to script from git root

--- .github/workflows/run_test_example.sh
+++ .github/workflows/run_test_example.sh
@@ -5,4 +5,4 @@
 alias syri='python <(echo "import syri.scripts.syri;syri.scripts.syri.main()")'
 
 # run using source to preserve alias
-source ../../example/example_workflow.sh
+source ./example/example_workflow.sh
