93bdfb1f166cd8483c85fe1f6e07887a1818592e
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-12-12 14:32:16 +0800

    try out test dependency installation

--- .github/workflows/run_test_example.sh
+++ .github/workflows/run_test_example.sh
@@ -3,6 +3,9 @@
 # alias to a call launching the syri entrypoint from python
 # necessary, as the hacky git install does not install the CLI entrypoints
 alias syri='python <(echo "import syri.scripts.syri;syri.scripts.syri.main()")'
-
+python <(echo "import syri.scripts.syri;syri.scripts.syri.main()") --version
+syri --version
+minimap2 --version
+minimap2.py --version
 # run using source to preserve alias
 source ./example/example_workflow.sh
