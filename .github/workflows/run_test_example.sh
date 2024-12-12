582d111902bb9ba176f5a97190e1774e83f51067
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2024-12-12 15:30:25 +0800

    switch to modifying code at runtime

--- .github/workflows/run_test_example.sh
+++ .github/workflows/run_test_example.sh
@@ -4,12 +4,16 @@
 # hacky way to hopefully alias the calls
 # normal alias does not seem to work in GitHub CI
 # necessary, as the hacky git install does not install the CLI entrypoints
-echo "#!/bin/bash python <(echo 'import syri.scripts.syri;syri.scripts.syri.main()')" > syri
-chmod +x ./syri
-echo "minimap2.py" > ./minimap2
-chmod +x ./minimap2
-PATH=$PATH:./
-syri --version
-minimap2
-# run using source to preserve alias
-source ./example/example_workflow.sh
+#echo "python <(echo 'import syri.scripts.syri;syri.scripts.syri.main()')" > syri
+#chmod +x ./syri
+#echo "minimap2.py" > ./minimap2
+#chmod +x ./minimap2
+#PATH=$PATH:./
+#syri --version
+#minimap2
+## run using source to preserve alias
+#source ./example/example_workflow.sh
+
+$CONDA/bin/conda install -c conda-forge -c bioconda "bioconda::minimap2"
+$(tail -n -1 ./example/example_workflow.sh |\
+	sed -e 's/^syri/python <(echo "import syri.scripts.syri;syri.scripts.syri.main()")/' )
