45220119d649e030dcf1f3d57af4503348256610
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-12-02 17:21:33 +0100

    add missing import

--- msyd/pyxfiles/io.pyx
+++ msyd/pyxfiles/io.pyx
@@ -20,7 +20,7 @@ import logging
 
 cimport numpy as np
 
-from msyd.coords import Range
+from msyd.coords import Range, read_psf_range
 from msyd.multisyn import Multisyn
 from msyd.vars import SNV
 import msyd.util as util
