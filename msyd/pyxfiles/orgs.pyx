66c3dabb47e72354b8d1716c5ce49d7f40b8b565
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-12-09 15:58:45 +0100

    further implement OrgContainer

--- msyd/pyxfiles/orgs.pyx
+++ msyd/pyxfiles/orgs.pyx
@@ -28,12 +28,25 @@ class OrgContainer:
         self.posdict = {} # track position of each ref; necessary?
 
     def get_n(self) -> int:
+        """
+        Returns the no. of organisms in this population.
+        """
         return self.n
 
     def get_orgs(self) -> List[Org]:
+        """
+        Returns the names of the organisms in this population, in order.
+        Meant to allow iteration across all organisms, and may change to a generator at any point.
+        To work with specific organism from this container, use `get_pos` or `get_at`.
+        """
         return self.orgs
 
-    def get_pos(self, org: Org) -> int:
+    def get_at(self, pos: int) -> Org:
+
+    def get_ind(self, org: Org) -> int:
+        """
+        Get the index of a given org in this OrgContainer.
+        """
         if not org in self.orgs:
             logger.error(f"OrgContainer queried with missing org: {org}")
             raise ValueError(f"OrgContainer queried with missing org: {org}")
@@ -44,6 +57,24 @@ class OrgContainer:
         self.posdict[org] = self.n
         self.orgs.append(org)
         self.n += 1
+
+    def sort(self):
+        """
+        Sorts this OrgContainer alphabetically.
+        Mutates self!
+        """
+        self.orgs = sorted(self.orgs)
+        self.posdict = {key:pos for pos, key in enumerate(self.orgs)}
+
+    # its convenient, but may confuse Cython code, so don't implement
+    #def __getitem__(self, key):
+    #    if isinstance(key, int):
+    #        return self.get_pos(key)
+    #    else:
+    #        return self.posdict[key]
+
+    def __contains__(self, org:Org):
+        return self.posdict.__contains__(org) # should be faster to ask the hashset than the list
 
     @classmethod
     def from_list(cls, lst):
