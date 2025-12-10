769a570e7a8acc73ae1f8dffd6ee56644ef0fda5
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-12-10 14:11:49 +0100

    simplify dict reconstruction

--- msyd/pyxfiles/multisyn.pyx
+++ msyd/pyxfiles/multisyn.pyx
@@ -483,7 +483,7 @@ cdef class ChromContainer:
         cdef list conts = list(self._backing.values())
         with multiprocessing.Pool(ncores) as pool:
             conts = pool.map(fn, conts)
-        return ChromContainer({chrom:cont for chrom, cont in zip(chroms, conts)})
+        return ChromContainer(dict(zip(chroms, conts))
 
 #THOUGHT: write NodeContainer/Graph class in syngraph that inherits from this?
 cdef class MultisynContainer:
