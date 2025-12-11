cde5984d3fa6248ed833ea563341c8f4e98fdc8e
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-12-11 11:44:47 +0100

    Chang match_synal back to work with syri DFs

--- msyd/pyxfiles/syri_handler.pyx
+++ msyd/pyxfiles/syri_handler.pyx
@@ -135,7 +135,7 @@ def extract_from_filelist(fins, qrynames, cores=1, **kwargs):
 
 # given a bam file and corresponding SYNAL range df,
 # Transform them into one list of Multisyn objects
-cpdef match_synal(syniter, alndf, ref='a'):
+cpdef match_synal(syndf, alndf, ref='a'):
     """
     This function takes an aligment and SYNAL dataframe and matches corresponding regions.
     It returns a dataframe containing the regions with the corresponding CIGAR string as a `Multisyn` object.
@@ -144,6 +144,7 @@ cpdef match_synal(syniter, alndf, ref='a'):
     """
     cdef:
         ret = MultisynContainer()
+        syniter = syndf.iterrows()
         alniter = alndf.iterrows()
         str refchr = ref + "chr"
         str refstart = ref + "start"
