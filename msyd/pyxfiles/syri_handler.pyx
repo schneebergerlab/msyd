35037c1be21fc5b8fd760c7185c8dd8ec7b5b9c6
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2026-05-12 17:26:03 +0200

    better error

--- msyd/pyxfiles/syri_handler.pyx
+++ msyd/pyxfiles/syri_handler.pyx
@@ -38,7 +38,7 @@ cpdef readsyriout(f):
     try:
         df = pd.DataFrame(list(syri_regs))[[0, 1, 2, 5, 6, 7, 10]]
     except KeyError:
-        raise ImportError("Incomplete input file {}, syri.out file should have 11 columns.".format(f))
+        raise ValueError("Incomplete input file {}, syri.out file should have 12 columns.".format(f))
     df = df.astype({0:'str', 5:'str', 10:'str',
                     1:'int64', 2:'int64', 6:'int64', 7:'int64', })
     # chr ID map
